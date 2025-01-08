3b9cdb0799558e8dd1559eeeeae0fb2cccdbd77f
Taiwo Oladosu <taiwooladosu1@gmail.com>
2025-01-08 12:46:35 +0100

    ui alignment

--- components/web/Projects.tsx
+++ components/web/Projects.tsx
@@ -15,94 +15,94 @@ import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 const Projects = () => {
   return (
      <div className="my-20 overflow-hidden" id="testimonials">
-        <h1 className="heading">
+        <h1 className="heading mb-14 md:mb-10">
             A small selection of{" "}
             <span className="text-purple-600"> recent Projects</span>
         </h1>
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-10">
           {myWorks.map((works) => (
-            <Modal key={works.id}>
-            <ModalTrigger>
-              <CardContainer className="inter-var">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[25rem] md:w-[20rem] lg:w-auto h-auto rounded-xl p-6 border  ">
-                  <CardItem translateZ="100" className="w-full mt-4">
-                    <Image
-                      src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                      height="1000"
-                      width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      alt="thumbnail"
-                    />
-                  </CardItem>
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
-                  >
-                    Hover over this card to unleash the power of CSS perspective
-                  </CardItem>
-                </CardBody>
-            </CardContainer> 
-            </ModalTrigger>
-            <ModalBody>
-              <ModalContent>
-                <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
-                  {works.title}
-                  {/* <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
-                    lorem
-                  </span> */}
-                </h4>
-                <div className="flex justify-center items-center">
-                  {works.images.map((image, idx) => (
-                    <motion.div
-                      key={"images" + idx}
-                      style={{
-                        rotate: Math.random() * 20 - 10,
-                      }}
-                      whileHover={{
-                        scale: 1.1,
-                        rotate: 0,
-                        zIndex: 100,
-                      }}
-                      whileTap={{
-                        scale: 1.1,
-                        rotate: 0,
-                        zIndex: 100,
-                      }}
-                      className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
-                    >
+              <Modal key={works.id} >
+              <ModalTrigger>
+                <CardContainer className="inter-var mt-[-100px]">
+                  <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[25rem] md:w-[20rem] lg:w-auto h-auto rounded-xl p-6 border  ">
+                    <CardItem translateZ="100" className="w-full mt-4">
                       <Image
-                        src={image}
-                        alt="bali images"
-                        width="500"
-                        height="500"
-                        className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                        src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        height="1000"
+                        width="1000"
+                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                        alt="thumbnail"
                       />
-                    </motion.div>
-                  ))}
-                </div>
-                <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
-                  
-                  {works.benefits.map((b, index) => (
-                    <div key={index} className="flex  items-center justify-center">
-                    <ElevatorIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
-                    <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                      {b}
-                    </span>
+                    </CardItem>
+                    <CardItem
+                      as="p"
+                      translateZ="60"
+                      className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center"
+                    >
+                      Hover over this card to unleash the power of CSS perspective
+                    </CardItem>
+                  </CardBody>
+              </CardContainer> 
+              </ModalTrigger>
+              <ModalBody>
+                <ModalContent>
+                  <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
+                    {works.title}
+                    {/* <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
+                      lorem
+                    </span> */}
+                  </h4>
+                  <div className="flex justify-center items-center">
+                    {works.images.map((image, idx) => (
+                      <motion.div
+                        key={"images" + idx}
+                        style={{
+                          rotate: Math.random() * 20 - 10,
+                        }}
+                        whileHover={{
+                          scale: 1.1,
+                          rotate: 0,
+                          zIndex: 100,
+                        }}
+                        whileTap={{
+                          scale: 1.1,
+                          rotate: 0,
+                          zIndex: 100,
+                        }}
+                        className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
+                      >
+                        <Image
+                          src={image}
+                          alt="bali images"
+                          width="500"
+                          height="500"
+                          className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                        />
+                      </motion.div>
+                    ))}
                   </div>
-                  ))}
-                </div>
-              </ModalContent>
-              <ModalFooter className="gap-4">
-                <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
-                  Cancel
-                </button>
-                <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
-                  Book Now
-                </button>
-              </ModalFooter>
-            </ModalBody>
-          </Modal>
+                  <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
+                    
+                    {works.benefits.map((b, index) => (
+                      <div key={index} className="flex  items-center justify-center">
+                      <ElevatorIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                      <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                        {b}
+                      </span>
+                    </div>
+                    ))}
+                  </div>
+                </ModalContent>
+                <ModalFooter className="gap-4">
+                  <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
+                    Cancel
+                  </button>
+                  <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
+                    Book Now
+                  </button>
+                </ModalFooter>
+              </ModalBody>
+              </Modal>
           ))}
         </div>
     </div>
